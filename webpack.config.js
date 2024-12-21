016dffc5d3ae87bfba5313c91ec6e20e16241253
nakul-chandak <chandak.nakul21@gmail.com>
2024-12-22 00:39:50 +0530

    Added the production Addin URL

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://office.azstage.guardrail.tech/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
