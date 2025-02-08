01dc8885fb37c2e5639ed150f650d85e25970b92
nakul-chandak <chandak.nakul21@gmail.com>
2025-02-09 01:37:09 +0530

    Fixed sign-in screen UI issue

--- src/taskpane/components/logIn/logIn.tsx
+++ src/taskpane/components/logIn/logIn.tsx
@@ -1,20 +1,19 @@
 import { Body1Strong, Button, Caption1, makeStyles } from '@fluentui/react-components'
 import React, { useEffect } from 'react'
-import backgroundIng from "../../../../assets/login-background.png"; 
+import backgroundIng from "../../../../assets/login-background.png";
 import componyLogo from "../../../../assets/guardrail-ai.png"
 import { useNavigate } from 'react-router-dom';
 
 const useStyles = makeStyles({
-  header1:{
-      height: "calc(100vh - 25vh)",
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundImage: `url(${backgroundIng})`,
-      padding: '20px'
+  header1: {
+    height: "calc(100vh - 30vh)",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundImage: `url(${backgroundIng})`
   },
-  landingLeft:{
+  landingLeft: {
     flex: 1,
     maxWidth: '80vh',
     textAlign: 'center',
@@ -41,19 +40,18 @@ function LogIn() {
   useEffect(() => {
     // Simulate a load event    
     const isAuthenticated = window.localStorage.getItem("LoggedIn");
-    if(isAuthenticated != undefined 
-      && isAuthenticated != '' 
-      && isAuthenticated != null 
-      && isAuthenticated === 'true')
-    {
+    if (isAuthenticated != undefined
+      && isAuthenticated != ''
+      && isAuthenticated != null
+      && isAuthenticated === 'true') {
       navigate('/home');
     }
   }, []);
-  const navigateSignUpPage = () =>{
+  const navigateSignUpPage = () => {
     navigate('/signup')
   }
   return (
-<>
+    <>
       <div className="ms-Grid">
 
         <div className="ms-Grid-row">
@@ -64,39 +62,42 @@ function LogIn() {
           </div>
         </div>
 
-        <div className= {` "ms-Grid-row" ${styles.header1}`}>
-        <div className="ms-Grid-col ms-sm12 ms-xl12">
-        <div className= "ms-Grid-row">
-          <div className="ms-Grid-col ms-sm12 ms-xl6">
+        <div className={` "ms-Grid-row" ${styles.header1}`} style={{ padding: '0px' }}>
+          <div className="ms-Grid-col ms-sm12 ms-xl12">
+            <div className="ms-Grid-row">
+              {/* <div className="ms-Grid-col ms-sm12 ms-xl6">
               <div id="landing-left" className={styles.landingLeft}>
                 <h1 style={{ lineHeight: '2rem' }}>
                   The most advanced integrated suite for AI productivity and protection
                 </h1>
               </div>
-          </div>
+          </div> */}
 
-          <div className="ms-Grid-col ms-sm12 ms-xl6">
-          <div id="landing-right" className={styles.landingRight}>
-                <h1>Get Started</h1>
+              <div className="ms-Grid-col ms-sm12 ms-xl6">
+                <div id="landing-right" className={styles.landingRight} style={{ lineHeight: '2rem' }}>
+                  <h2 style={{ margin: '0px' }}>The most advanced integrated suite for AI productivity and protection</h2>
+                </div>
+                <div id="landing-right" className={styles.landingRight}>
+                  <h2>Get Started</h2>
+                </div>
+                <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '15px' }}>
+                  <Button appearance="primary" size='small' type="submit" name="login" onClick={navigateSigInPage}>
+                    Sign In
+                  </Button>
+                  <Button appearance="primary" size='small' type="submit" name="createAccount" onClick={navigateSignUpPage}>
+                    Create Account
+                  </Button>
+                </div>
               </div>
-              <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: '15px' }}>
-              <Button appearance="primary" size='small' type="submit" name="login" onClick={navigateSigInPage}>
-                Sign In
-              </Button>
-              <Button appearance="primary" size='small' type="submit" name="createAccount" onClick={navigateSignUpPage}>
-                Create Account
-              </Button>
             </div>
-          </div>
           </div>
         </div>
       </div>
-      </div>
 
-  <div style={{ textAlign: 'center', marginTop: '10px', padding: '10px' }}>
-    <Caption1>Guardrail Technologies Inc., ©2025</Caption1>
-  </div>
-</>
+      <div style={{ textAlign: 'center', marginTop: '10px', padding: '10px' }}>
+        <Caption1>Guardrail Technologies Inc., ©2025</Caption1>
+      </div>
+    </>
 
   )
 }
